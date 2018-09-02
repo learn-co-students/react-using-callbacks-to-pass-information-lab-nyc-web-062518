d4d6423fae1850a46dd77437d606fb2ad55ce9b4
Daniel Ran <danielr1735@gmail.com>
2018-09-01 23:24:10 -0400

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import Matrix from './Matrix.js'
 
 export default class ColorSelector extends Component {
-  
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div
+          key={idx}
+          className="color-swatch"
+          style={{backgroundColor: str}}
+          onClick={e => this.handleClick(e)}/>
     })
   )
-  
+
+  handleClick(e) {
+    let color = e.target.style.backgroundColor
+    return this.props.triggerMatrixMethod(color)
+  }
+
   render() {
+    console.log(this.props)
     return (
-      <div id="colorSelector">
+      <div id="colorSelector" >
         {this.makeColorSwatches()}
       </div>
     )
   }
-  
+
 }
